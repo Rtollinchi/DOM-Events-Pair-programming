6ebdfba1edca700b98ed3be0f94949239de8a7dc
Rtollinchi <rtollinchi23@gmail.com>
2022-12-14 16:55:48 -0500

    added magnifiying glass to github

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ cookieButton.addEventListener("click", function(){
     let clickCounter = document.querySelector("#clickCount");
     clickCounter.textContent = counter;
 })
+
+let magButton = document.querySelector("#magnifying");
+let magMin = document.querySelector("#minimize");
+let paragraph = document.querySelector("#text");
+let size = 15
+magButton.addEventListener("click", function () {
+    size += 20;
+   paragraph.style.fontSize=size + "px";
+   if (size >= 100){
+    size = 100
+    
+   }console.log(size)
+})
+magMin.addEventListener('click', function () {
+    size--;
+    paragraph.style.fontSize=size + "px";
+    if (size <= 1){
+        size = 1
+    } console.log(size)
+})
+
+
+
