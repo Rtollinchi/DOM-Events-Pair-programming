30f93e3acaa9b38e9a8c04db779d99d99a9d30e4
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2022-12-14 19:00:51 -0500

    finished greenscreen practice

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let magMin = document.querySelector("#minimize");
 let paragraph = document.querySelector("#text");
 let size = 15
 magButton.addEventListener("click", function () {
-    size += 20;
+    size += 1;
    paragraph.style.fontSize=size + "px";
    if (size >= 100){
     size = 100
@@ -27,5 +27,16 @@ magMin.addEventListener('click', function () {
     } console.log(size)
 })
 
+let colorButton = document.body.querySelector("#background");
+let userInput = document.body.querySelector("#colorSelected");
+// console.log(userInput);
+// let color = "white";
+// document.body.style.backgroundColor = color;
+// const body = document.body.querySelector('body');
+colorButton.addEventListener('click', function onClick(event){
+    event.preventDefault();
+    document.body.style.backgroundColor = userInput.value;
 
+})
+// console.log(color);
 
